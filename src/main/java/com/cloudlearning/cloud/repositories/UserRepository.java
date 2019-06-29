56d757d30294d6690e82aa86b634ab4fe39a7d6a
Stefan Simic <stefan.simic.twint@interventure.info>
2019-06-29 15:53:09 +0200

    Add suport for soft delete Add auto fielde for create and update timestamp

--- src/main/java/com/cloudlearning/cloud/repositories/UserRepository.java
+++ src/main/java/com/cloudlearning/cloud/repositories/UserRepository.java
@@ -1,8 +1,9 @@
 package com.cloudlearning.cloud.repositories;
 
 import com.cloudlearning.cloud.models.security.User;
+import com.cloudlearning.cloud.repositories.Base.SoftDelete.SoftDeleteCrudRepository;
+import com.cloudlearning.cloud.repositories.Base.SoftDelete.SoftDeletePagingAndSortingRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -11,11 +12,12 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long>, PagingAndSortingRepository<User, Long> {
+public interface UserRepository extends SoftDeleteCrudRepository<User, Long>, SoftDeletePagingAndSortingRepository<User, Long> {
 
     @Query("SELECT DISTINCT user FROM User user " +
             "INNER JOIN FETCH user.authority AS authority " +
-            "WHERE user.username = :username")
+            "WHERE user.username = :username"
+            + " AND user.isDeleted = false")
     Optional<User> findByUsername(@Param("username") String username);
 
     List<User> findByUsernameOrId(@Param("username") String username, @Param("id") Long id);
